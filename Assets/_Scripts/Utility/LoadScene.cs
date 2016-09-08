85c51e786386aac558ebe2d3480f016d582ce583
Cam McKasson <cam@mckassons.com>
2016-09-07 17:04:43 -0700

    added basic menus; started working on cube...

---
+++ Assets/_Scripts/Utility/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour
+{
+	public void Load (int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void Load (string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
