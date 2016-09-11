f403188a58b936c216dc2b79a8cbdec507dec1eb
Cam McKasson <cam@mckassons.com>
2016-09-11 13:47:39 -0700

    still working on the cube...

---
+++ Assets/_Scripts/VRubiksCubeController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VRubiksCubeUserInput))]
+public class VRubiksCubeController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Turn(GameObject touched, Vector2 move)
+    {
+        //Front/top/bottom or side...
+        float upCheck, forwardCheck, rightCheck;
+        upCheck = Vector3.Dot(touched.transform.up, transform.up);
+        forwardCheck = Vector3.Dot(touched.transform.up, transform.forward);
+        rightCheck = Vector3.Dot(touched.transform.up, transform.right);
+
+        bool front = false, side = false;
+        if (Mathf.Abs(upCheck) >= 0.9f || Mathf.Abs(forwardCheck) >= 0.9f)
+        {
+            front = true;
+        }
+        else if (Mathf.Abs(rightCheck) >= 0.9f)
+        {
+            side = true;
+        }
+
+        Debug.Log("front/top/bottom == " + front.ToString() + " ; side == " + side.ToString());
+
+        //Center touch?
+        if (touched.transform.parent.gameObject.tag == "center")
+        {
+            if (front)
+            {
+
+            }
+
+            if (side)
+            {
+
+            }
+        }
+        else
+        {
+            if (front)
+            {
+
+            }
+
+            if (side)
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/VRubiksCubeUserInput.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(VRubiksCubeController))]
+public class VRubiksCubeUserInput : MonoBehaviour
+{
+    public float m_minMove;
+
+    private VRubiksCubeController m_cubeController;
+
+    private GameObject m_touched;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_cubeController = GetComponent<VRubiksCubeController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetTouchInput();	
+	}
+
+    private void GetTouchInput ()
+    {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+                RaycastHit hit;
+
+                // If ray hit
+                if (Physics.Raycast(ray, out hit))
+                {                    
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("TouchPanel"))
+                    {
+                        //Valid touch
+                        m_touched = hit.collider.gameObject;
+
+                        Debug.Log("m_touched == " + m_touched.name);
+
+                        //Add touch noise and "visualization" stuff here
+
+                        //Watch for valid move
+                        StartCoroutine(GetMoveInput());
+                        return;
+                    }
+                }                    
+            }
+        }
+    }
+
+    IEnumerator GetMoveInput ()
+    {
+        Vector2 touchStart = Input.GetTouch(0).position;
+
+        Vector2 move;
+
+        while(Input.touchCount >= 0)
+        {
+            move = Input.GetTouch(0).position - touchStart;
+
+            if (move.magnitude > m_minMove)
+            {
+                //Send m_touched and move to VRubiksCubeController
+                m_cubeController.Turn(m_touched, move);
+
+                //Exit loop
+                break;
+            }
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
