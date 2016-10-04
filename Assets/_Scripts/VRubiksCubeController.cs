627ded61aebd17227c5925ecf211d80187817368
Cam McKasson <cam@mckassons.com>
2016-10-03 17:04:08 -0700

    need to check logic for face turning...

--- Assets/_Scripts/VRubiksCubeController.cs
+++ Assets/_Scripts/VRubiksCubeController.cs
@@ -7,9 +7,11 @@ public class VRubiksCubeController : MonoBehaviour
 {
     private VRubiksCubeUserInput m_userInput;
 
-    private bool m_rotatingCube = false;
+    private bool m_rotatingCube = false, m_rotatingFace = false;
 
     private List<GameObject> m_cubes, m_rotationGroup;
+
+    private GameObject m_faceRotator;
 
 	// Use this for initialization
 	void Start ()
@@ -30,6 +32,11 @@ public class VRubiksCubeController : MonoBehaviour
         }
 
         //Debug.Log("m_cubes.count == " + m_cubes.Count.ToString());
+
+        m_faceRotator = new GameObject();
+        m_faceRotator.transform.position = transform.position;
+        m_faceRotator.transform.rotation = transform.rotation;
+        m_faceRotator.transform.parent = transform;
 	}
 	
 	// Update is called once per frame
@@ -243,9 +250,7 @@ public class VRubiksCubeController : MonoBehaviour
         
         //
         //
-        // TODO: WRITE A FUNCTION FOR "FINDING ROTATION GROUP"
-        //
-        // TODO: WRITE A FUNCTION FOR ROTATING ROTATION GROUP (SIMILAR TO ROTATING WHOLE CUBE)
+        // CHECK LOGIC (MAYBE TEST ROTATIONS WITH NO LOGIC???)!!!
         //
         //
 
@@ -256,19 +261,19 @@ public class VRubiksCubeController : MonoBehaviour
                 // Use Y coord to select group
                 float tarCoord = touched.transform.parent.localPosition.y;
 
-                //Debug.Log("tarCoords == " + tarCoord.ToString());
+                Debug.Log("tarCoords == " + tarCoord.ToString());
 
                 m_rotationGroup.Clear();
 
                 foreach (GameObject cube in m_cubes)
                 {
-                    if (cube.transform.localPosition.y == tarCoord)
+                    if (cube.transform.localPosition.y <= tarCoord + 0.1f && cube.transform.localPosition.y >= tarCoord - 0.1f)
                     {
                         m_rotationGroup.Add(cube);
                     }
                 }
 
-                //Debug.Log("m_rotationGroup.count == " + m_rotationGroup.Count.ToString());
+                Debug.Log("m_rotationGroup.count == " + m_rotationGroup.Count.ToString());
             }
             else if(top || bottom)
             {
@@ -281,7 +286,7 @@ public class VRubiksCubeController : MonoBehaviour
 
                 foreach (GameObject cube in m_cubes)
                 {
-                    if (cube.transform.localPosition.z == tarCoord)
+                    if (cube.transform.localPosition.z <= tarCoord + 0.1f && cube.transform.localPosition.z >= tarCoord - 0.1f)
                     {
                         m_rotationGroup.Add(cube);
                     }
@@ -301,7 +306,7 @@ public class VRubiksCubeController : MonoBehaviour
 
                 foreach (GameObject cube in m_cubes)
                 {
-                    if (cube.transform.localPosition.x == tarCoord)
+                    if (cube.transform.localPosition.x <= tarCoord + 0.1f && cube.transform.localPosition.x >= tarCoord - 0.1f)
                     {
                         m_rotationGroup.Add(cube);
                     }
@@ -318,7 +323,7 @@ public class VRubiksCubeController : MonoBehaviour
 
                 foreach (GameObject cube in m_cubes)
                 {
-                    if (cube.transform.localPosition.z == tarCoord)
+                    if (cube.transform.localPosition.z <= tarCoord + 0.1f && cube.transform.localPosition.z >= tarCoord - 0.1f)
                     {
                         m_rotationGroup.Add(cube);
                     }
@@ -332,6 +337,13 @@ public class VRubiksCubeController : MonoBehaviour
             if (front || left || right)
             {
                 // Group rotates CW around Y
+                Quaternion tarRot = Quaternion.Euler(0.0f, 90.0f, 0.0f) * m_faceRotator.transform.localRotation;
+
+                do
+                {
+                    RotateCubeFaceRotate(tarRot);
+                    yield return null;
+                } while (m_rotatingFace);
             }
             else if (top)
             {
@@ -391,4 +403,101 @@ public class VRubiksCubeController : MonoBehaviour
         m_rotatingCube = false;
         yield return null;
     }
+
+    void RotateCubeFaceRotate (Quaternion tarRot)
+    {
+        // Set rotation group parent transform on first call
+        if (!m_rotatingFace)
+        {
+            // Reset rotator
+            //m_faceRotator.transform.rotation = transform.rotation;
+            //m_faceRotator.transform.localRotation = Quaternion.identity;
+
+            foreach (GameObject cube in m_rotationGroup)
+            {
+                cube.transform.parent = m_faceRotator.transform;
+            }
+        }
+        m_rotatingFace = true;        
+
+        // Rotate
+        m_faceRotator.transform.localRotation = Quaternion.Slerp(m_faceRotator.transform.localRotation, tarRot, 0.1f);
+        
+        if (Quaternion.Angle(m_faceRotator.transform.localRotation, tarRot) <= 1.0f)
+        {
+            // Finalize rotation
+            m_faceRotator.transform.localRotation = tarRot;
+
+            // Re-parent cubes
+            foreach (GameObject cube in m_rotationGroup)
+            {
+                cube.transform.parent = transform;
+            }            
+
+            m_rotatingFace = false;                        
+        }
+    }
 }
+
+
+/*
+        //use angle between now and tarRot to generate slerp angle then use transform.rotate instead...
+        Quaternion thisRot = Quaternion.Slerp(transform.localRotation, tarRot, 0.1f);
+
+        foreach (GameObject cube in m_rotationGroup)
+        {            
+            cube.transform.localRotation = thisRot;
+            //cube.transform.localPosition = thisRot * transform.localPosition;
+
+            //DEBUG_loopCount++;
+            if (Quaternion.Angle(transform.localRotation, tarRot) <= 1.0f)
+            {
+                cube.transform.localRotation = tarRot;
+
+                Vector3 tarPos = cube.transform.localPosition;
+
+                if (tarPos.x > 0.5f)
+                {
+                    tarPos.x = 1.0f;
+                }
+                else if (tarPos.x < -0.5f)
+                {
+                    tarPos.x = -1.0f;
+                }
+                else
+                {
+                    tarPos.x = 0.0f;
+                }
+
+                if (tarPos.y > 0.5f)
+                {
+                    tarPos.y = 1.0f;
+                }
+                else if (tarPos.y < -0.5f)
+                {
+                    tarPos.y = -1.0f;
+                }
+                else
+                {
+                    tarPos.y = 0.0f;
+                }
+
+                if (tarPos.z > 0.5f)
+                {
+                    tarPos.z = 1.0f;
+                }
+                else if (tarPos.z < -0.5f)
+                {
+                    tarPos.z = -1.0f;
+                }
+                else
+                {
+                    tarPos.z = 0.0f;
+                }
+
+                //cube.transform.localPosition = tarPos;
+
+                //Debug.Log("RotateWholeCubeRotate() DEBUG_loopCount == " + DEBUG_loopCount.ToString());
+                //DEBUG_loopCount = 0;            
+            }
+        }*/
